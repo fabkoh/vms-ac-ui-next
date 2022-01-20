5cd5e7a86a605a13b807d92d0f2a9a57c57033d0
fabkoh <43871370+fabkoh@users.noreply.github.com>
2022-01-20 12:07:31 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/next.config.js /home/koh_fabian/runNextTmp
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/package.json /home/koh_fabian/runNextTmp
  cd /home/koh_fabian/runNextTmp
+ pkill "npm run start"
+ pkill node
  nohup npm run start &> next.out &
  exit      
 EOF
