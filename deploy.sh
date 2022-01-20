9f1cb9e5aee2bfc116997842f7e21aaa88ed20bf
fabkoh <43871370+fabkoh@users.noreply.github.com>
2022-01-20 11:49:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 -#!/bin/sh
- npm ci
- npm run build
+ sudo npm ci
+ sudo npm run build
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/.next/ /home/koh_fabian/runNextTmp
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/node_modules/ /home/koh_fabian/runNextTmp
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/next.config.js /home/koh_fabian/runNextTmp
  cp -r /var/lib/jenkins/workspace/vms-ac-ui-next/package.json /home/koh_fabian/runNextTmp
- npm run start
+ cd /home/koh_fabian/runNextTmp
+ nohup npm run start &> next.out &
  exit      
 EOF
