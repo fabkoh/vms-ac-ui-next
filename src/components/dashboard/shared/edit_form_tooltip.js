777e0423d622bc62b762143d80bc75f180597a68
ong-wei-hong <weihong.ong@outlook.com>
2022-02-25 10:01:59 +0800

    fixed bug with EditFormToolTip

--- src/components/dashboard/shared/edit_form_tooltip.js
+++ src/components/dashboard/shared/edit_form_tooltip.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ClickAwayListener, Tooltip } from "@mui/material";
 import Info from "@mui/icons-material/Info";
 
-const EditFormToolTIp = ({ handleTooltipClose, handleTooltipOpen, open}) => {
+const EditFormToolTIp = () => {
     
     const [open, setOpen] = useState(false);
     const handleTooltipOpen = () => setOpen(true);
