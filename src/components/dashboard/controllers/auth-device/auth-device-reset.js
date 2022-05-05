660eb4d77441bcc3b389c3d74ff75ae5231a18bd
jieelinnchew <jielin.chew@isssecurity.sg>
2022-05-05 14:17:14 +0800

    [WIP] delete and reset auth devices

--- src/components/dashboard/controllers/auth-device/auth-device-reset.js
+++ src/components/dashboard/controllers/auth-device/auth-device-reset.js
@@ -1,218 +1,100 @@
-import WarningAmberOutlined from "@mui/icons-material/WarningAmberOutlined";
-import { Alert, Box, Button, Checkbox, Dialog, DialogContent, DialogContentText, DialogTitle, Step, StepLabel, Stepper, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material"
-import { useState } from "react";
-import { Scrollbar } from "../../../scrollbar";
+import * as React from "react";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import {
+	Button,
+	Box,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	TextField
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import WarningAmberOutlinedIcon from "@mui/icons-material/WarningAmberOutlined";
+import Alert from "@mui/material/Alert";
 
-const steps = ['Select authentication devices to reset', 'Confirm Reset'];
+const AuthDeviceReset = (props) => {
+	const { open, handleDialogClose, resetAuthDevices } = props;
 
-const AuthDeviceReset = ({ open, authDevices, handleDialogClose, resetAuthDevices }) => {
+	//text field
+	const [value, setValue] = useState("");
+	const handleTextChange = (e) => setValue(e.target.value);
+	const resetDisabled = value != 'RESET';
 
-    // stepper
-    const [activeStep, setActiveStep] = useState(0)
-    const handleNextStep = () => {
-        setValue("");
-        setActiveStep(activeStep + 1);
-    }
-    const handlePrevStep = () => {
-        setValue("");
-        setActiveStep(activeStep - 1);
-    }
+	// closing actions
+	const handleClose = () => { 
+		handleDialogClose();
+		setValue("");
+	}
 
-    // table select
-    const [selected, setSelected] = useState([])
-    const selectedAll = Array.isArray(authDevices) && selected.length == authDevices.length;
-    const selectedSome = selected.length > 0 && !selectedAll;
-    const handleSelectAll = (e) => setSelected(e.target.checked ? authDevices.map(authDevice => authDevice.authDeviceId) : []); //TODO: check if auth device has an id
-    const handleSelectFactory = (id) => () => {
-        if (selected.includes(id)) {
-            setSelected(selected.filter(i => i != id));
-        } else {
-            setSelected([ ...selected, id ]);
-        }
-    }
-    const nextDisabled = selected.length == 0;
+	// delete action
+	const handleResetAuthDevices = () => {
+		resetAuthDevices();
+		handleClose();
+	}
 
-    // text field
-    const [value, setValue] = useState("");
-    const handleTextChange = (e) => setValue(e.target.value);
-    const resetDisabled = value != 'RESET';
+	return (
+		<>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				onBackdropClick={handleClose}
+			>
+				<DialogTitle>
+					{" "}
+					<WarningAmberOutlinedIcon
+						sx={{ color: "#F44336", m: -0.5, width: 50 }}
+					/>{" "}
+					&#8288;Confirm Reset?
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						Are you sure you want to reset authentication device(s)? This action cannot
+						be undone.
+					</DialogContentText>
+					<DialogContentText>
+						Selected authentication devices will be reset to their default settings.
+					</DialogContentText>
 
-    // closing actions
-    const handleClose = () => { 
-        handleDialogClose();
-        setActiveStep(0);
-        setSelected([]);
-    }
+					<form onSubmit={handleResetAuthDevices}>
+						<TextField
+							variant="filled"
+							fullWidth
+							helperText="Please type in RESET to continue"
+							onChange={handleTextChange}
+							autoFocus
+						/>
 
-    // delete action
-    const handleResetAuthDevices = () => {
-        resetAuthDevices(selected, selectedAll);
-        handleClose();
-    }
+						<Box display="flex" justifyContent="flex-end" mt={1}>
+							<Button
+								color="error"
+								disabled={resetDisabled}
+								variant="contained"
+								sx={{ borderRadius: 8, marginRight: 1}}
+								onClick={() => {
+									resetAuthDevices();
+									props.setActionAnchor(null);
+								}}
+							>
+							    Reset
+							</Button>
 
-
-    return (
-        <Dialog 
-            open={open}
-            onClose={handleClose}
-            onBackdropClick={handleClose}
-            fullWidth
-        >
-            <Box padding={2}>
-                <Stepper activeStep={activeStep}>
-                    {
-                        steps.map(step => (
-                            <Step key={step}>
-                                <StepLabel>{step}</StepLabel>
-                            </Step>
-                        ))
-                    }
-                </Stepper>
-            </Box>
-            {
-                activeStep == 0 ? (
-                    // render authDevice select
-                    <>
-                        <DialogTitle>
-                            Select Authentication Devices to reset
-                        </DialogTitle>
-                        <DialogContent>
-                            <Scrollbar>
-                                <Table>
-                                    <TableHead sx={{ backgroundColor: "neutral.200" }}>
-                                        <TableRow>   
-                                            <TableCell padding="checkbox">
-                                                <Checkbox
-                                                    checked={selectedAll}
-                                                    indeterminate={selectedSome}
-                                                    onChange={handleSelectAll}
-                                                />
-                                            </TableCell>
-                                            <TableCell>Name</TableCell>
-                                            <TableCell>Direction</TableCell>
-                                            <TableCell>Last Online</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {
-                                            Array.isArray(authDevices) && (
-                                                authDevices.map((authDevice, i) => {
-                                                    const {
-                                                        authDeviceId,
-                                                        authDeviceName,
-                                                        authDeviceDirection,
-                                                        lastOnline,
-                                                        masterPin
-                                                    } = authDevice;
-                                                    const isAuthDeviceSelected = selected.includes(authDeviceId);
-                                                    const handleSelect = handleSelectFactory(authDeviceId);
-                                                        
-                                                    return (
-                                                        <TableRow hover key={i}>
-                                                            <TableCell padding="checkbox">
-                                                                <Checkbox
-                                                                    checked={isAuthDeviceSelected}
-                                                                    value={isAuthDeviceSelected}
-                                                                    onChange={handleSelect}
-                                                                />
-                                                            </TableCell>
-                                                            <TableCell>
-                                                                { authDeviceName }
-                                                            </TableCell>
-                                                            <TableCell>
-                                                                { authDeviceDirection }
-                                                            </TableCell>
-                                                            <TableCell>
-                                                                { lastOnline }
-                                                            </TableCell>
-                                                        </TableRow>
-                                                    )
-                                                })
-                                            )
-                                        }
-                                    </TableBody>
-                                </Table>
-                            </Scrollbar>
-                            <Box display="flex" justifyContent="space-between" mt={1}>
-                                <Button 
-                                    variant="outlined"
-                                    sx={{ borderRadius: 8 }}
-                                    color="error"
-                                    onClick={handleClose}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button 
-                                    onClick={handleNextStep} 
-                                    disabled={nextDisabled}
-                                    variant="contained"
-                                    sx={{ borderRadius: 8 }}
-                                >
-                                    Next
-                                </Button>
-                            </Box>
-                        </DialogContent>
-                    </>
-                ) : (
-                    // render reset confirm
-                    <>
-                        <DialogTitle>
-                            {" "}
-                            <WarningAmberOutlined
-                                color="error"
-                                sx={{
-                                    color: "F44336",
-                                    m: -0.5, 
-                                    width: 50
-                                }}
-                            />
-                            {" "}
-                            &#8288;Confirm Reset?
-                        </DialogTitle>
-                        <DialogContent>
-                            <DialogContentText sx={{ whiteSpace: "pre-line", mb: 1}}>
-                                Are you sure you want to reset authentication device(s)? This action cannot be undone.
-                                
-                                \nSelected authentication devices will be reset to their default settings.
-                                {/*
-                                    selectedAll && 
-                                    <strong>{"\nDeleting all schedules would remove this entrance from the access group"}</strong>
-                                */}
-                            </DialogContentText>
-                            <form onSubmit={handleResetAuthDevices}>
-                                <TextField
-                                    variant="filled"
-                                    fullWidth
-                                    onChange={handleTextChange}
-                                    helperText="Please Type in RESET to continue"
-                                    value={value}
-                                    autoFocus
-                                />
-                                <Box display="flex" justifyContent="space-between" mt={1}>
-                                    <Button 
-                                        onClick={handlePrevStep}
-                                        variant="outlined"
-                                        sx={{ borderRadius: 8 }}
-                                    >
-                                        Back
-                                    </Button>
-                                    <Button 
-                                        type="submit"
-                                        color="error" 
-                                        disabled={resetDisabled}
-                                        variant="contained"
-                                        sx={{ borderRadius: 8 }}
-                                    >
-                                        Reset
-                                    </Button>
-                                </Box>
-                            </form>
-                        </DialogContent>                                    
-                    </>
-                )
-            }
-        </Dialog>
-    )
+							<Button
+								onClick={() => {
+									handleClose();
+									props.setActionAnchor(null);
+								}}
+								variant="outlined"
+								sx={{ borderRadius: 8, color: "main.primary" }}
+							>
+								Cancel
+							</Button>		
+						</Box>		
+					</form>
+				</DialogContent>
+			</Dialog>
+		</>
+	);
 }
 
 export default AuthDeviceReset;
