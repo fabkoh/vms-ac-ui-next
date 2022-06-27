f87d04d1288af604d075c44e9edc94e39bd73d9a
Yong Ning <yongning.lee@isssecurity.com>
2022-06-27 13:34:38 +0800

    pre-sprint6

---
+++ src/api/events.js
@@ -0,0 +1,13 @@
+import { useApi, fakePersons, fakeAccessGroups } from './api-config';
+import { sendApi } from './api-helpers';
+
+class EventslogsApi {
+
+
+    getEvents() {
+        if (useApi) { return sendApi('/api/events'); }
+        return Promise.resolve(new Response(JSON.stringify(controllers), { status: 200 }));
+    }
+}
+
+export const eventslogsApi = new EventslogsApi();
