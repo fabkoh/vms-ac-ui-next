af007bcb8016882fdfce1d1cd2de2c5d7b2b220a
Yong Ning <yongning.lee@isssecurity.com>
2022-06-28 16:35:27 +0800

    merge

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -44,30 +44,3 @@ const toDisplayEventsDateString = (str) => {
 
 export { isObject, filterByState, stringIn, arraySameContents, DEFAULT_URL, toDisplayDateString,toDisplayEventsDateString }
 
-{
-    title: t('Events'),
-    items: [
-      {
-        title: t('Management'),
-        path: '/dashboard/persons',
-        icon: <NotificationImportantIcon fontSize="small" />,
-        children: [
-          {
-            title: t('Add'),
-            path: '/dashboard/persons/create'
-          }
-        ]
-      },
-      {
-        title: t('Logs'),
-        path: '/dashboard/logs',
-        icon: <DescriptionIcon fontSize="small" />,
-        children: [
-          {
-            title: t('List'),
-            path: '/dashboard/logs'
-          }
-        ]
-      },
-    ]
-  },
