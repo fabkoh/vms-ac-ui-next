656b3e3550f20753657714402d6b9689c62cff93
Yong Ning <yongning.lee@isssecurity.com>
2022-06-28 17:06:50 +0800

    merge

---
+++ src/utils/utils.js
@@ -0,0 +1,38 @@
+<<<<<<< .merge_file_a08460
+=======
+const isObject = (e) => typeof e === 'object' && e !== null
+
+// converts filter by string to filter by state (uses state.inputValue)
+const filterByState = (filterFunction) => (v, state) => filterFunction(v, state.inputValue);
+
+// obj can be anything, case insensitive search
+const stringIn = (s, obj) => typeof(obj) === 'string' && obj.toLowerCase().includes(s);
+
+// compares arr1 and arr2 contents
+const arraySameContents = (arr1, arr2) => {
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+
+    const arr1Obj = {};
+    arr1.forEach(i => arr1Obj[i] == true);
+    
+    return arr2.every(i => arr1Obj[i]);
+}
+
+// default URL (see getControllerDetailsLink in ./controller)
+const DEFAULT_URL = '/dashboard';
+
+const months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov' , 'Dec'];
+// takes in a raw date string eg 2022-05-06T13:09:14.372126 and converts to display date string eg 6 May 2022 13:09
+const toDisplayDateString = (str) => {
+    return (
+        str.slice(8, 10) + ' ' +
+        months[Number(str.slice(5, 7))] + ' ' +
+        str.slice(0, 4) + ', ' +
+        str.slice(11, 16)
+    );
+};
+
+export { isObject, filterByState, stringIn, arraySameContents, DEFAULT_URL, toDisplayDateString }
+>>>>>>> .merge_file_a20876
