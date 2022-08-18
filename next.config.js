32aca2a75b7b7045be18f474ac855440e7258bff
Alissa <alissayarmantho1@gmail.com>
2022-08-18 17:26:29 +0800

    Config changes (ignore eslint)

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const withTM = require('next-transpile-modules')([
 ]);
 
 module.exports = withTM({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
