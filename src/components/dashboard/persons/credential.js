d6ba83f2a60f25564ea77890045a568842433ecf
Alissa <alissayarmantho1@gmail.com>
2022-08-19 10:28:21 +0800

    [VS-478] Add cred validations and error mapping

--- src/components/dashboard/persons/credential.js
+++ src/components/dashboard/persons/credential.js
@@ -57,7 +57,7 @@ const Credential = ({ onCredTypeChange, credTypes, credential, removeCredential,
     }
 
     const TTLHelperText = (endDate != null && endDate < today) ? "Note: expiry is before today" : "Expiry date is end inclusive";
-    const credentialInUse = validation.credentialInUseIds.includes(credId);
+    const credentialCheckFailed = validation.credentialCheckFailed[credId] !== undefined;
     const credentialRepeated = validation.credentialRepeatedIds.includes(credId);
 	const credentialUidRepeatedForNotPinTypeCred = validation.credentialUidRepeatedIds.includes(credId);
 
@@ -106,9 +106,9 @@ const Credential = ({ onCredTypeChange, credTypes, credential, removeCredential,
                         label="Value"
                         handleChange={handleCredUidChange}
                         inputRef={credUidRef}
-                        error={credentialInUse || credentialRepeated || credentialUidRepeatedForNotPinTypeCred}
+                        error={ credentialRepeated || credentialCheckFailed || credentialUidRepeatedForNotPinTypeCred}
                         helperText={
-                            (credentialInUse && "Error: credential type and value in use") ||
+                            (credentialCheckFailed && "Error: " + validation.credentialCheckFailed[credId]) ||
                             (credentialRepeated && "Error: repeated credential type & value in form") ||
                             (credentialUidRepeatedForNotPinTypeCred && "Error: credential value for non-pin credentials must be unique") || ' '
                         }
