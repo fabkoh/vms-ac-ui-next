f8361a796eef733a745b2ad71cb36b302f676ff4
WFWA TECH <tusharinani93@gmail.com>
2022-10-22 01:13:07 +0530

    Updated: the Api(s)

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -3,23 +3,37 @@ const createAuthRequest = async (path, data) => {
     try {
         const token = localStorage.getItem("accessToken");
         const auth = token ? { "Authorization": `bearer ${token}` } : {};
-        const response = await (
-            await fetch(`${api}/${path}`, {
+        const response = await fetch(`${api}/${path}`, {
                 method: "POST",  
                 headers: {
                     "Content-Type": "application/json",
                     ...auth
                 },
                 body: JSON.stringify(data)
-            })
-        ).json();
-        return { response: response, type: "success" };
+            });
+    return { response: await response.json(), type: response.ok ? "success" : "error"};
     } catch (error) {
-        return { response: error?.message, type: "error" };
+        return { response: error, type: "error" };
     }
   };
 
-export const authLogin = async({ email, password}) => {
+  const createGetRequest = async (path) => {
+    try {
+        const token = localStorage.getItem("accessToken");
+        const response = await fetch(`${api}/${path}`, {
+                method: "GET",  
+                headers: {
+                    Authorization: `Bearer ${token}`                    
+                }
+            })
+        
+        return { response: await response.json(), type: response.ok ? "success" : "error"};
+    } catch (error) {
+        return { response: error, type: "error" };
+    }
+  };
+
+  export const authLogin = async({ email, password}) => {
     const res = await createAuthRequest("api/auth/signin", { email, password });
     if(res.type === "success") { 
         localStorage.setItem("accessToken", res.response.accessToken)
@@ -35,6 +49,15 @@ export const authRenewToken = async() => {
 
 
 export const authLogOut = async() => {
-    const res = await createPostRequest("api/auth/signout");
+    const res = await createAuthRequest("api/auth/signout");
     if(res.type === "success") { localStorage.removeItem("accessToken"); }
+}
+
+export const authSignUp = async (data) => {
+    const res = await createAuthRequest("api/auth/signup",data);
+    return res;
+}
+export const authGetProfile = async () => {
+    const res = await createGetRequest("api/auth/profile");
+    return res;
 }
