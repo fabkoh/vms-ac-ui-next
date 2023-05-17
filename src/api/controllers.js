014f7bb37b7feee0402b136490a81808036caee2
bicky <bickybong@gmail.com>
2023-05-17 16:35:52 +0800

    updated controller delete

--- src/api/controllers.js
+++ src/api/controllers.js
@@ -1,96 +1,121 @@
-import toast from 'react-hot-toast';
-import { useApi, fakeAccessGroups, fakePersons , fakeAccessGroupSchedule, fakeControllers, fakeAuthDevices } from './api-config';
-import { encodeArrayForSpring, sendApi } from './api-helpers';
+import toast from "react-hot-toast";
+import {
+  useApi,
+  fakeAccessGroups,
+  fakePersons,
+  fakeAccessGroupSchedule,
+  fakeControllers,
+  fakeAuthDevices,
+} from "./api-config";
+import { encodeArrayForSpring, sendApi } from "./api-helpers";
 
 class ControllerApi {
-
-    getControllers() {
-        if (useApi) { 
-            return sendApi(`/api/controllers`);
-        }
-        const controllers = fakeControllers.map(controller => {
-            return{...controller}
-        })
-        controllers.forEach(controller => {
-                // populate authDevice
-                controller.authDevice = fakeAuthDevices.filter(device => device.controllerId == controller.controllerId);
-
-            return controller
-        })
-        return Promise.resolve(new Response(JSON.stringify(controllers), { status: 200 }));
+  getControllers() {
+    if (useApi) {
+      return sendApi(`/api/controllers`);
     }
-    
-    getController(controllerId) {
-        if (useApi) { 
-            return sendApi(`/api/controller/${controllerId}`);
-        }
-        const controller = fakeControllers.find(c => c.controllerId==controllerId)
+    const controllers = fakeControllers.map((controller) => {
+      return { ...controller };
+    });
+    controllers.forEach((controller) => {
+      // populate authDevice
+      controller.authDevice = fakeAuthDevices.filter(
+        (device) => device.controllerId == controller.controllerId
+      );
 
-        if(controller){
-            controller.authDevices = fakeAuthDevices.filter(d=>d.controllerId==controllerId)
-            return Promise.resolve(new Response(JSON.stringify(controller), { status: 200 }));
-        }
+      return controller;
+    });
+    return Promise.resolve(
+      new Response(JSON.stringify(controllers), { status: 200 })
+    );
+  }
 
-        return Promise.resolve(new Response(
-            JSON.stringify({ personId: `controller with Id ${controllerId} does not exist` }),
-            { status: 404 }
-        ));
+  getController(controllerId) {
+    if (useApi) {
+      return sendApi(`/api/controller/${controllerId}`);
     }
+    const controller = fakeControllers.find(
+      (c) => c.controllerId == controllerId
+    );
 
-    updateController({
-        controllerId,
-        controllerIP,
-        controllerName,
-        controllerIPStatic,
-        controllerMAC,
-        controllerSerialNo,
-    }) {
-        if (useApi) {
-            return sendApi(`/api/controller/${controllerId}`, 
-                {
-                    method: 'PUT',
-                    headers: {
-                        'Content-type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        controllerId,
-                        controllerIP,
-                        controllerName,
-                        controllerIPStatic,
-                        controllerMAC,
-                        controllerSerialNo,
-                    })
-                }
-            );
-        }
+    if (controller) {
+      controller.authDevices = fakeAuthDevices.filter(
+        (d) => d.controllerId == controllerId
+      );
+      return Promise.resolve(
+        new Response(JSON.stringify(controller), { status: 200 })
+      );
     }
 
-    
+    return Promise.resolve(
+      new Response(
+        JSON.stringify({
+          personId: `controller with Id ${controllerId} does not exist`,
+        }),
+        { status: 404 }
+      )
+    );
+  }
 
-    deleteController(controllerId) {
-        if (useApi) { return sendApi(`/api/controller/delete/${controllerId}`, { method: 'DELETE' }); }
+  updateController({
+    controllerId,
+    controllerIP,
+    controllerName,
+    controllerIPStatic,
+    controllerMAC,
+    controllerSerialNo,
+  }) {
+    if (useApi) {
+      return sendApi(`/api/controller/${controllerId}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          controllerId,
+          controllerIP,
+          controllerName,
+          controllerIPStatic,
+          controllerMAC,
+          controllerSerialNo,
+        }),
+      });
     }
-    
-    getAuthStatus(controllerId){
-        if(useApi){return sendApi(`/api/controllerConnection/${controllerId}`)}
-    }
-    
-    resetController(controllerId) {
-        if (useApi) { return sendApi(`/api/controller/reset/${controllerId}`, { method: 'DELETE' }); }
-    }
+  }
 
-    uniconUpdater() {
-        if (useApi) {
-            return sendApi(`/api/uniconUpdater`, { method: 'POST' })
-        }
+  deleteController(controllerId) {
+    if (useApi) {
+      return sendApi(`/api/controller/delete/${controllerId}`, {
+        method: "DELETE",
+      });
     }
-    
-    getAllCurrentAuthMethod(controllerId){
-        if(useApi){return sendApi(`/api/controller/currentAuthMethod/${controllerId}`)}
+  }
+
+  getAuthStatus(controllerId) {
+    if (useApi) {
+      return sendApi(`/api/controllerConnection/${controllerId}`);
     }
+  }
 
+  resetController(controllerId) {
+    if (useApi) {
+      return sendApi(`/api/controller/reset/${controllerId}`, {
+        method: "PUT",
+      });
+    }
+  }
 
+  uniconUpdater() {
+    if (useApi) {
+      return sendApi(`/api/uniconUpdater`, { method: "POST" });
+    }
+  }
 
+  getAllCurrentAuthMethod(controllerId) {
+    if (useApi) {
+      return sendApi(`/api/controller/currentAuthMethod/${controllerId}`);
+    }
+  }
 }
 
 export const controllerApi = new ControllerApi();
