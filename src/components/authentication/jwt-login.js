b75e55a273685551452429de0afb04dffae4a8ce
junnengsoo <soojunneng01@gmail.com>
2023-05-17 16:39:27 +0800

    Add commented out refreshTokenChecker polling with component unMount() issue

--- src/components/authentication/jwt-login.js
+++ src/components/authentication/jwt-login.js
@@ -4,11 +4,62 @@ import { useFormik } from 'formik';
 import { Alert, Box, Button, FormHelperText, TextField } from '@mui/material';
 import { useAuth } from '../../hooks/use-auth';
 import { useMounted } from '../../hooks/use-mounted';
+import { useEffect, useState } from "react";
+import { sendApi } from "../../api/api-helpers";
 
 export const JWTLogin = (props) => {
   const isMounted = useMounted();
   const router = useRouter();
   const { login } = useAuth();
+  const [isPolling, setIsPolling] = useState(false);
+
+  useEffect(
+    () => {
+      let timer;
+      console.log("refreshToken useEffect");
+      console.log(isPolling);
+
+      if (isPolling) {
+        console.log("isPolling is true")
+        timer = setInterval(() => {
+          refreshTokenChecker();
+        }, 5 * 1000);
+      } else {
+        return () => clearInterval(timer);
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isPolling]
+  );
+
+  
+
+  const refreshTokenChecker = async () => {
+    try {
+      console.log("refreshTokenChecker");
+
+      const response = await sendApi("/api/auth/refreshTokenChecker", {
+        method: "POST",
+        body: JSON.stringify({ refreshToken: localStorage.getItem("refreshToken") })
+      },
+        false);
+
+      if (response.status === 404) {
+        console.log("refreshTokenChecker 404");
+        // Stop the interval when refreshTokenChecker API returns 404
+        setIsPolling(false);
+
+        // // Redirect to login page
+        router.replace("");
+      }
+    } catch (error) {
+      console.error("Error here")
+      console.error("Error:", error);
+      // Stop the interval when an error occurs
+      setIsPolling(false);
+    }
+  }
+
   const formik = useFormik({
     initialValues: {
       email: 'ISSAdmin@isssecurity.sg',
@@ -28,8 +79,10 @@ export const JWTLogin = (props) => {
     }),
     onSubmit: async (values, helpers) => {
       const res = await login(values.email, values.password);
-      if(res.type === "success"){
+      if (res.type === "success") {
         if (isMounted()) {
+          console.log("JWTLogin onSubmit");
+          setIsPolling(true);
           const returnUrl = router.query.returnUrl || '/dashboard';
           router.push(returnUrl);
         }
@@ -39,7 +92,7 @@ export const JWTLogin = (props) => {
         helpers.setErrors({ submit: res.response.message });
         helpers.setSubmitting(false);
       }
-      
+
     }
   });
 
