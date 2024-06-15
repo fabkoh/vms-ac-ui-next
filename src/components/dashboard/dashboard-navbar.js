374c4bed4c14001bce0a45877f5b40d55c941bb8
Yong Ning <yongning.lee@isssecurity.com>
2024-06-15 17:08:20 +0800

    able to npm run build ocally

--- src/components/dashboard/dashboard-navbar.js
+++ src/components/dashboard/dashboard-navbar.js
@@ -14,14 +14,8 @@ import {
 import { styled } from '@mui/material/styles';
 import { Menu as MenuIcon } from '../../icons/menu';
 import { AccountPopover } from './account-popover';
-import { ContactsPopover } from './contacts-popover';
-import { ContentSearchDialog } from './content-search-dialog';
-import { NotificationsPopover } from './notifications-popover';
 import { LanguagePopover } from './language-popover';
-import { Bell as BellIcon } from '../../icons/bell';
 import { UserCircle as UserCircleIcon } from '../../icons/user-circle';
-import { Search as SearchIcon } from '../../icons/search';
-import { Users as UsersIcon } from '../../icons/users';
 
 const languages = {
   en: '/static/icons/uk_flag.svg',
@@ -83,112 +77,6 @@ const LanguageButton = () => {
       <LanguagePopover
         anchorEl={anchorRef.current}
         onClose={handleClosePopover}
-        open={openPopover}
-      />
-    </>
-  );
-};
-
-const ContentSearchButton = () => {
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const handleOpenSearchDialog = () => {
-    setOpenDialog(true);
-  };
-
-  const handleCloseSearchDialog = () => {
-    setOpenDialog(false);
-  };
-
-  return (
-    <>
-      <Tooltip title="Search">
-        <IconButton
-          onClick={handleOpenSearchDialog}
-          sx={{ ml: 1 }}
-        >
-          <SearchIcon fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <ContentSearchDialog
-        onClose={handleCloseSearchDialog}
-        open={openDialog}
-      />
-    </>
-  );
-};
-
-const ContactsButton = () => {
-  const anchorRef = useRef(null);
-  const [openPopover, setOpenPopover] = useState(false);
-
-  const handleOpenPopover = () => {
-    setOpenPopover(true);
-  };
-
-  const handleClosePopover = () => {
-    setOpenPopover(false);
-  };
-
-  return (
-    <>
-      <Tooltip title="Contacts">
-        <IconButton
-          onClick={handleOpenPopover}
-          sx={{ ml: 1 }}
-          ref={anchorRef}
-        >
-          <UsersIcon fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <ContactsPopover
-        anchorEl={anchorRef.current}
-        onClose={handleClosePopover}
-        open={openPopover}
-      />
-    </>
-  );
-};
-
-const NotificationsButton = () => {
-  const anchorRef = useRef(null);
-  const [unread, setUnread] = useState(0);
-  const [openPopover, setOpenPopover] = useState(false);
-  // Unread notifications should come from a context and be shared with both this component and
-  // notifications popover. To simplify the demo, we get it from the popover
-
-  const handleOpenPopover = () => {
-    setOpenPopover(true);
-  };
-
-  const handleClosePopover = () => {
-    setOpenPopover(false);
-  };
-
-  const handleUpdateUnread = (value) => {
-    setUnread(value);
-  };
-
-  return (
-    <>
-      <Tooltip title="Notifications">
-        <IconButton
-          ref={anchorRef}
-          sx={{ ml: 1 }}
-          onClick={handleOpenPopover}
-        >
-          <Badge
-            color="error"
-            badgeContent={unread}
-          >
-            <BellIcon fontSize="small" />
-          </Badge>
-        </IconButton>
-      </Tooltip>
-      <NotificationsPopover
-        anchorEl={anchorRef.current}
-        onClose={handleClosePopover}
-        onUpdateUnread={handleUpdateUnread}
         open={openPopover}
       />
     </>
